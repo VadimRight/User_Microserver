ddc1c93187c6a238a5749eb16f1f948c28f4d3c9
VadimRight <vadimright@mail.ru>
2024-05-10 13:32:31 +0300

    New type

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,22 +21,47 @@ type Config struct {
 }
 
 
-func EnvLoad() Config {
-	env := os.Getenv("ENV")
-	log := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ldate)
-	log.Printf("ENV is %s", env)
-	configPath := os.Getenv("CONFIG_PATH")
-	if configPath == "" {
-		log.Fatal("Failed to read config path")
-	}
-	log.Printf("CONFIG PATH is %s", configPath)
-	err := godotenv.Load(configPath)
-	if err != nil {
-		log.Fatalf("Error while loadig .env file: %v", err)
-	}
-	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		log.Fatalf("config does not exists: %s", err)
-	}	
-	var cfg Config
-	return cfg
+
+func EnvLoad() *Config {
+  log := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ldate)
+
+  err := godotenv.Load(".env")
+  if err != nil {
+    log.Fatalf("Error while loadig .env file: %v", err)
+  }
+
+  env, ok := os.LookupEnv("ENV")
+  if !ok {
+    log.Fatal("Failed to read ENV")
+  }
+
+  configPath, ok := os.LookupEnv("CONFIG_PATH")
+  if !ok {
+    log.Fatal("Failed to read CONFIG_PATH")
+  }
+
+  _, err = os.Stat(configPath)
+  if os.IsNotExist(err) {
+    log.Fatalf("config does not exists: %s", err)
+  }
+
+  log.Println(configPath)
+
+  postgresPort, ok := os.LookupEnv("POSTGRES_PORT")
+  if !ok {
+    log.Fatal("Failed to read POSTGRES_PORT")
+  }
+
+  return &Config{
+    Postgres_Port:     postgresPort,
+    Postgres_Host:     "",
+    Database_Name:     "",
+    Postgres_User:     "",
+    Postgres_Password: "",
+    Env:               env,
+    Server_Address:    "",
+    Server_Port:       "",
+    Timeout:           0,
+    IdleTimeout:       0,
+  }
 }
