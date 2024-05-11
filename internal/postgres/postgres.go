3d18109e4a7c96420cf130e3f890e06ef39c6cdd
VadimRight <vadimright@mail.ru>
2024-05-12 02:08:13 +0300

    Fix: now initialize database is working

--- internal/postgres/postgres.go
+++ internal/postgres/postgres.go
@@ -14,21 +14,20 @@ type PostgresStorage struct {
 
 func InitPostgresDatabase() *PostgresStorage {
 	postgresCfg := config.LoadPostgresConfig()
-	postgresUrl := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",postgresCfg.Postgres_Host, postgresCfg.Postgres_Port, postgresCfg.Postgres_User, postgresCfg.Postgres_Password, postgresCfg.Database_Name)
-
+	postgresUrl := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",postgresCfg.Postgres_Host, postgresCfg.Postgres_Port, postgresCfg.Postgres_User, postgresCfg.Postgres_Password, postgresCfg.Database_Name)	
 	db, err := sql.Open("postgres", postgresUrl)
 	if err != nil {
-		log.Fatalf("Error while connecting to postgres databse: %v", err)	
+		log.Fatalf("Error while connecting to postgres database: %v", err)	
 	}
 
 	createDatabase, err := db.Prepare(`
-	CREATE TABLE IF NOT EXISTS user(
-		id UUID,
-		username VARCHAR(20),
-		email VARCHAR(20),
-		password BINARY,
-		is_verified BOOL DEFAULT 0
-		is_activate BOOL DEFAULT 0
+	CREATE TABLE IF NOT EXISTS "user" (
+		id UUID PRIMARY KEY,
+		username VARCHAR(20) NOT NULL UNIQUE,
+		email VARCHAR(20) NOT NULL UNIQUE,
+		password CHAR(60) NOT NULL UNIQUE,
+		is_verified BOOL NOT NULL DEFAULT false,
+		is_activate BOOL NOT NULL DEFAULT false
 	);`)
 	if err != nil {
 		log.Fatalf("Error while creating user table: %v", err)
