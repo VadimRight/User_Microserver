078e40d5a9e4252803beae29b87e4ce8942e04ff
VadimRight <vadimright@mail.ru>
2024-05-12 01:25:44 +0300

    Build: init database postgres is called in main package

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,23 @@
 package main
 
+
 import (
+	"log"
 	"log/slog"
 	"github.com/VadimRight/User_Microserver/pkg/prettylogger/handler/logger"
 	"github.com/VadimRight/User_Microserver/internal/config"
+	"github.com/VadimRight/User_Microserver/internal/postgres"
 )
 
 func main() {
 	cfg := config.LoadConfig()
-	log := logger.SetupPrettyLogger(cfg.Env.Env)
-	log.Info(
+	logPretty := logger.SetupPrettyLogger(cfg.Env.Env)
+	database := postgres.InitPostgresDatabase()
+	logPretty.Info(
 		"starting url-shortener",
 		slog.String("env", cfg.Env.Env),
 	)
-	log.Info(
+	logPretty.Info(
 		"this is log",
 		slog.String("Postgres Name", cfg.Postgres.Database_Name),
 		slog.String("Postgres Port", cfg.Postgres.Postgres_Port),
@@ -24,4 +28,5 @@ func main() {
 		slog.Duration("Timeout", cfg.Server.Timeout),
 		slog.Duration("Idle Timeout", cfg.Server.IdleTimeout),
 	)
+	log.Printf("Database is %v", database)	
 }
