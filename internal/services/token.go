86b8fca43270727cc7789c665b2128b8ac02b0ea
VadimRight <vadimright@mail.ru>
2024-07-01 16:59:23 +0300

    Fit: token generation and validation service

---
+++ internal/services/token.go
@@ -0,0 +1,65 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// Структура токенов Bearer, используемых в данном приложении
+type JwtCustomClaim struct {
+	Id string `json:"id"`
+	jwt.StandardClaims
+}
+
+// Получение JWT секрета из .env
+var jwtSecret = []byte(getJwtSecret())
+
+func getJwtSecret() string {
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		return "aSecret"
+	}
+	return secret
+}
+
+type AuthService struct{}
+
+func NewAuthService() AuthService {
+	return AuthService{}
+}
+
+// AuthService интерфейс для работы с JWT
+type TokenVaditor interface {
+	GenerateToken(ctx context.Context, userID string) (string, error)
+	ValidateToken(ctx context.Context, token string) (*jwt.Token, error)
+}
+
+func (s *AuthService) GenerateToken(ctx context.Context, userId string) (string, error) {
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, &JwtCustomClaim{
+		Id: userId,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+	})
+
+	token, err := t.SignedString(jwtSecret)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+func (s *AuthService) ValidateToken(ctx context.Context, token string) (*jwt.Token, error) {
+	return jwt.ParseWithClaims(token, &JwtCustomClaim{}, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("there's a problem with the signing method")
+		}
+		return jwtSecret, nil
+	})
+}
