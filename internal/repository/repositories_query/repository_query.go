a9557aafa371feb93bd8121889c386e1d5f2033c
VadimRight <vadimright@mail.ru>
2024-07-07 00:44:57 +0300

    Fit: added sql files for selecting user by name and selecting all users

--- internal/repository/repositories_query/repository_query.go
+++ internal/repository/repositories_query/repository_query.go
@@ -1,8 +1,16 @@
 package repositories_query
 
+import (
+	_ "embed"
+)
+
 var (
+	// go:embed repositories_query/select_by_name.sql
 	GetUserByUsername string
-	InsertUser        string
-	GetUserByID       string
-	GetAllUsers       string
+	// go:embed repositories_query/insert.sql
+	InsertUser string
+	// go:embed repositories_query/select.sql
+	GetUserByID string
+	// go:embed repositories_query/select_all.sql
+	GetAllUsers string
 )
