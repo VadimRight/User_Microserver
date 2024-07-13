d0f68ab0bbd6210b4a6ac835296a048788403dcc
VadimRight <vadimright@mail.ru>
2024-07-13 22:37:53 +0300

    Fit: func Conflict for displaying conflicts on requests and AppError which implements error inteface

--- internal/domain/errors/errors.go
+++ internal/domain/errors/errors.go
@@ -1,8 +1,29 @@
 package errors
 
-import "errors"
+import (
+	"errors"
+	"net/http"
+)
+
+type AppError struct {
+	Code    int
+	Err     error
+	Message string
+}
+
+func (e *AppError) Error() string {
+	return e.Err.Error()
+}
 
 var (
 	ErrUsernameAlreadyExists = errors.New("user with this username already exists")
 	ErrEmailAlreadyExists    = errors.New("user with this email already exists")
 )
+
+func Conflict(err error) error {
+	return &AppError{
+		Code:    http.StatusConflict,
+		Message: "Conflict",
+		Err:     err,
+	}
+}
