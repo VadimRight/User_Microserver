3a0fb07653f3fb3f82dfe0e6d0710eafae54f828
VadimRight <vadimright@mail.ru>
2024-07-18 20:59:44 +0300

    Fit: usecase for user registration is up

--- internal/domain/usecase/user_usecase.go
+++ internal/domain/usecase/user_usecase.go
@@ -23,11 +23,19 @@ type UserRegisterRequestValidator interface {
 	ValidateUserRegisterRequest() error
 }
 
-func (u *userUsecase) RegisterUser(ctx context.Context, payload dtos.UserRegisterRequest) (userId entity.Uuid, err error) {
+type PasswordHasher interface {
+	HashPassword(password string) (string, error)
+}
+
+func (u *userUsecase) RegisterUser(ctx context.Context, payload dtos.UserRegisterRequest, passwdService PasswordHasher) (userId entity.Uuid, err error) {
 	if exists := u.repo.IsUserExist(ctx, payload.Username); exists {
 		return userId, errors.Conflict(errors.ErrUsernameAlreadyExists)
 	}
 	userId.GenerateNewId()
-	user, err = u.repo.InsertUser(ctx, userId, payload.Username)
-	return userId, nil
+	hashedPasswd, err := passwdService.HashPassword(payload.Password)
+	if err != nil {
+		return entity.Uuid{}, err
+	}
+	user, err := u.repo.InsertUser(ctx, userId, payload.Username, hashedPasswd)
+	return user.Id, nil
 }
